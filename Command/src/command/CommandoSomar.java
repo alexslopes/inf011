0cce4948e2b32d98d6a5b2d7b62ebd6264263c98
Alex <alexstlopes@gmail.com>
2017-09-27 20:15:44 -0300

    Adicionado Command

---
+++ Command/src/command/CommandoSomar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command;
+
+import interfaces.Command;
+
+/**
+ *
+ * @author aluno
+ */
+public class CommandoSomar implements Command{
+
+    @Override
+    public void Execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
