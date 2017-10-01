d3e6df1aaa063565d3db9f6a527285a81dae3d18
Alex <alexstlopes@gmail.com>
2017-09-30 22:13:46 -0300

    Completar compararNumeros()

--- Iterator/src/Iterator/IteratorSuperPower.java
+++ Iterator/src/Iterator/IteratorSuperPower.java
@@ -31,17 +31,15 @@ public class IteratorSuperPower implements IIterator {
         this.objeto = objeto;
         this.operator = operator;
         this.nota = nota;
-    }   
-    
-    public Object verificadorDeMetodo(Object objeto, String metodo) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException{
+    }
+
+    public Object verificadorDeMetodo(Object objeto, String metodo) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException {
         Class clazz = Class.forName(objeto.getClass().getName());
         Method metodoDoSeuObjeto = clazz.getMethod("get" + StringUtils.capitalize(metodo));
         Object retornoDoMetodo = metodoDoSeuObjeto.invoke(objeto);
         return retornoDoMetodo;
 
     }
-    
-    
 
     @Override
     public void first() {
@@ -49,12 +47,16 @@ public class IteratorSuperPower implements IIterator {
     }
 
     @Override
-    public Aluno next() {
+    public Object next() {
+        Object objetoNext = this.objetos.get(cont + 1);
         try {
-            Object metodo = verificadorDeMetodo(this.objetos.get(cont), objeto);
-            if(metodo instanceof Double){
-                return compararNumeros(metodo);
-            }else if (metodo instanceof Double){
+            Object metodo = verificadorDeMetodo(objetoNext, objeto);
+            if (metodo instanceof Integer) {
+                if(compararNumeros((Integer) metodo))
+                    return objetoNext;
+                else
+                    return objetoNext;
+            } else if (metodo instanceof Double) {
                 return compararString(metodo);
             }
         } catch (InvocationTargetException ex) {
@@ -66,24 +68,64 @@ public class IteratorSuperPower implements IIterator {
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(IteratorSuperPower.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return null;
+    }
+
+    public boolean compararNumeros(int num) {
+        int numAtual;
+        try {
+            numAtual = (Integer) verificadorDeMetodo(currentItem(), objeto);
+            if (this.operator.equals(">")) {
+                if (num > numAtual) 
+                    return true;
+                else
+                    return false;
+                
+            } else if (this.operator.equals("<")) {
+
+            } else if (this.operator.equals("==")) {
+
+            } else if (this.operator.equals(">=")) {
+
+            } else if (this.operator.equals("<=")) {
+
+            } else {
+
+            }
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(IteratorSuperPower.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(IteratorSuperPower.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(IteratorSuperPower.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(IteratorSuperPower.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Double compararNumeros(double num) {
+
     }
 
     @Override
     public boolean isDone() {
-        return objetos.size() == cont; 
+        return objetos.size() == cont;
     }
 
     @Override
     public boolean hasNext() {
-       return (objetos.size() - 1) != cont; 
+        return (objetos.size() - 1) != cont;
     }
 
     @Override
-    public Aluno currentItem() {
-        
+    public Object currentItem() {
+        if (isDone()) {
+            cont = objetos.size() - 1;
+        } else if (cont < 0) {
+            cont = 0;
+        }
+        return objetos.get(cont);
     }
-
 
 }
